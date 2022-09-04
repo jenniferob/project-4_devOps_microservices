204b1b6a2a34cb37b666f89d95a88c8064f8cfb8
EC2 Default User <ec2-user@ip-172-31-41-216.ec2.internal>
2022-09-04 00:18:47 +0000

    project4 deploy

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="jenniferob12/prj4micro"
 # Step 2:  
 # Authenticate & tag
+docker tag prj4micro $dockerpath
 echo "Docker ID and Image: $dockerpath"
-
+docker login
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
